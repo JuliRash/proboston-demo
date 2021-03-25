2166b4d188fd87384b2677b1317091143701f152
julius idowu i.o <idowujulius92@gmail.com>
2021-03-25 20:33:41 +0100

    modify models.py file to display string model instance

--- example/models.py
+++ example/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 
-class User(models.Model):
+class UserInformation(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, blank=True)
     ico = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.name
