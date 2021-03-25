91b7d69c8e08076a093defe405ad7280cd382553
julius idowu i.o <idowujulius92@gmail.com>
2021-03-25 20:32:54 +0100

    modify view to save to the database

--- example/views.py
+++ example/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
+from example.forms import UserInformationForm
+from example.models import UserInformation
 
 
 def index(request):
-    return HttpResponse("Hello, World")
+    form = UserInformationForm()
+    if request.method == 'POST':
+        form = UserInformationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'User Information saved successfully to the database')
+            form = UserInformationForm()
+    return render(request, 'index.html', {'form': form})
